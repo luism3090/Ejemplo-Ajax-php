7d4230798c2a0e2a130d083189321ec811661ccb
Luis Molina <luisame@outlook.com>
2017-01-11 13:56:22 -0600

    pasar datos de javascrip ajax a php

---
+++ ejemploEnvio.php
@@ -0,0 +1,12 @@
+<?php
+
+
+var_dump($_POST["dato1"]);
+
+//var_dump($_POST["dato2"]);
+
+
+var_dump ($_POST["dato1"][0]["nombreCompleto"]);
+var_dump ($_POST["dato1"][0]["edad"]);
+var_dump ($_POST["dato1"][0]["telefono"]);
+
